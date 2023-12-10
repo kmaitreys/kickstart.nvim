a8fe4a6851f60e172f412cec26834f619df14b73
kmaitreys <peakcipher1729@gmail.com>
2023-12-10 20:35:21 +0530

    started lsp setup

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+local plugins = {{
+    "williamboman/mason.nvim",
+    opts = {
+        ensure_installed = {"pyright"}
+    }
+}}
+return plugins
